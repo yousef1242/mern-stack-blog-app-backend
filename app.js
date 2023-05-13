4aca18801f17de6c619af0dc6553450c2d09f0cf
yousef <yousefahmedelshahed@gmail.com>
2023-05-13 20:40:07 +0300

    change url of frontend

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
         "http://localhost:3000",
-        "https://mern-stack-blog-app.onrender.com"
+        "https://mern-stack-blog-seven.vercel.app/"
     ],
   })
 );
